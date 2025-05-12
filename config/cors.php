c21354711a5b9e0d4ce3f14ab080fdd01d529ea5
Abdulrahman hilal <abdrahmanmhran3@gmail.com>
2025-05-13 02:12:26 +0300

    ارسال بريد ترحيب

--- config/cors.php
+++ config/cors.php
@@ -1,22 +1,11 @@
 <?php
 
 return [
-
     'paths' => ['api/*'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'], // اسمح بكل النطاقات (للتجريب)
-
-    'allowed_origins_patterns' => [],
-
+    'allowed_origins' => ['*'],
     'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
     'supports_credentials' => false,
-
 ];
+
 
